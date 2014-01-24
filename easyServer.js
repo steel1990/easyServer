2dfda13079cb75aa756e92b04d81d669c2bcd389
steel1990 <lbbsteel@gmail.com>
2014-01-24 18:14:59 +0800

    add uri decode

--- easyServer.js
+++ easyServer.js
@@ -1,47 +1,48 @@
 #! /usr/bin/env node
 
-var http = require("http"),
-	Url = require("url"),
-	fs = require("fs"),
-	mimeType = require("./mimeType.js"),
-	argv = process.argv.slice(2),
-	basePath = process.cwd(),
-	port = 8099;
+var http = require("http");
+var Url = require("url");
+var fs = require("fs");
+var mimeType = require("./mimeType.js");
+var argv = process.argv.slice(2);
+var basePath = process.cwd();
+var port = 8099;
 
 if(basePath.charAt(basePath.length - 1) !== '\\'){
-	basePath += "\\";
+    basePath += "\\";
 }
 
 for(var i = 0; i < argv.length; i += 1){
-	if(!isNaN(parseInt(argv[i]))){
-		port = parseInt(argv[i]);
-	}else if(/^\\\//.test(argv[i])){
-		basePath = basePath.replace(/^([a-zA-Z]:)/, "$1") + argv[i];
-	}else{
-		basePath += argv[i];
-	}
+    if(!isNaN(parseInt(argv[i]))){
+        port = parseInt(argv[i]);
+    }else if(/^\\\//.test(argv[i])){
+        basePath = basePath.replace(/^([a-zA-Z]:)/, "$1") + argv[i];
+    }else{
+        basePath += argv[i];
+    }
 }
 
 http.createServer(function (request, response) {
-	var pathname = Url.parse(request.url).pathname;
-	console.log(pathname);
-	if(pathname.charAt(pathname.length - 1) === '/'){
-		pathname += "index.html";
-	}
-	getFile(response, basePath + pathname);
+    var pathname = Url.parse(request.url).pathname;
+    pathname = decodeURIComponent(pathname);
+    console.log(pathname);
+    if(pathname.charAt(pathname.length - 1) === '/'){
+        pathname += "index.html";
+    }
+    getFile(response, basePath + pathname);
 }).listen(port);
 
 console.log("server start at " + port + "\nThe basePath is " + basePath);
 
 function getFile(res, path){
-	fs.readFile(path, function (err, data) {
-		if(err){
-			res.writeHead(404);
-			res.end(JSON.stringify(err));
-			return;
-		}
-		var t = path.replace(/.*\.([\w]+)$/, "$1");
-		res.writeHead(200, { 'Content-Type': mimeType[t.toLowerCase()] || mimeType["html"] });
-		res.end(data);
-	});
+    fs.readFile(path, function (err, data) {
+        if(err){
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        var t = path.replace(/.*\.([\w]+)$/, "$1");
+        res.writeHead(200, { 'Content-Type': mimeType[t.toLowerCase()] || mimeType["html"] });
+        res.end(data);
+    });
 }
